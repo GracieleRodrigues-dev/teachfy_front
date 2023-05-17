5b2d8150adbdf70699b10a7c98e1e676d2056afc
Graciele Rodrigues <graciele_rodrigues@outlook.com.br>
2023-05-17 08:08:16 -0300

    tentando mexer no css. AAAAAAAAAAAAAAH

--- teachfy/src/pages/SignUpPage.tsx
+++ teachfy/src/pages/SignUpPage.tsx
@@ -1,5 +1,6 @@
 import { SignUpForm } from '../components/SignUpForm';
 import { Link } from 'react-router-dom';
+import '../styles/signup.css'; // Importe o arquivo CSS
 
 export function SignUpPage() {
   const handleRegister = (name: string, email: string, password: string) => {
@@ -12,7 +13,8 @@ export function SignUpPage() {
   return (
     <div className="signup-page">
       <div className="signup-container">
-        <h2>Cadastro</h2>
+        <h2>Cadastro</h2>        
+        <p>Inscreva-se para começar</p>        
         <SignUpForm onSubmit={handleRegister} />
         <div className="signup-links">
           <Link to="/login">Já possui uma conta? Login</Link>
