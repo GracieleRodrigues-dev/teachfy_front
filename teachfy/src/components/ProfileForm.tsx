5448a2858cc06e4f95d7afe5c131dfe252bcce0c
Graciele Rodrigues <graciele_rodrigues@outlook.com.br>
2023-05-20 15:57:10 -0300

    Sidebar,ProfilePage, etc

---
+++ teachfy/src/components/ProfileForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+interface ProfileFormProps {
+  onSubmit: (name: string, email: string) => void;
+  name: string;
+  email: string;
+}
+
+export const ProfileForm: React.FC<ProfileFormProps> = ({ onSubmit, name, email }) => {
+  const [updatedName, setUpdatedName] = useState(name);
+  const [updatedEmail, setUpdatedEmail] = useState(email);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(updatedName, updatedEmail);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          id="name"
+          value={updatedName}
+          onChange={(e) => setUpdatedName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          value={updatedEmail}
+          onChange={(e) => setUpdatedEmail(e.target.value)}
+        />
+      </div>
+      {/* <button type="submit">Alterar Dados</button> */}
+    </form>
+  );
+};
--- teachfy/src/pages/SignUpPage.tsx
+++ teachfy/src/pages/SignUpPage.tsx
@@ -1,6 +1,6 @@
 import { SignUpForm } from '../components/SignUpForm';
 import { Link } from 'react-router-dom';
-import '../styles/signup.css'; // Importe o arquivo CSS
+import '../styles/signup.css';
 
 export function SignUpPage() {
   const handleRegister = (name: string, email: string, password: string) => {
@@ -11,14 +11,16 @@ export function SignUpPage() {
   };
 
   return (
-    <div className="signup-page">
-      <div className="signup-container">
-        <h2>Cadastro</h2>        
-        <p>Inscreva-se para começar</p>        
-        <SignUpForm onSubmit={handleRegister} />
-        <div className="signup-links">
-          <Link to="/login">Já possui uma conta? Login</Link>
-          <Link to="/forgot-password"> Esqueceu a senha?</Link>
+    <div>
+      <div className="signup-page">
+        <div className="signup-container">
+          <h2>Cadastro</h2>        
+          <p>Inscreva-se para começar</p>        
+          <SignUpForm onSubmit={handleRegister} />
+          <div className="signup-links">
+            <Link to="/login">Já possui uma conta? Login</Link>
+            <Link to="/forgot-password"> Esqueceu a senha?</Link>
+          </div>
         </div>
       </div>
     </div>
