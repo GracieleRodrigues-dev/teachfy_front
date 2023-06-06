6f25117f4bebe5861fdfa3883625b77cfa2afab5
Graciele Rodrigues <graciele_rodrigues@outlook.com.br>
2023-06-05 22:41:25 -0300

    Criei a page /profile que lista os dados do usuário logado, já está funcionado conexão com o back, só precisa ajustar para pegar os dados do usuário logado. Também estou criando o modal para conseguir alterar nome, e-mail e senha.

--- teachfy/src/components/ProfileForm.tsx
+++ teachfy/src/components/ProfileForm.tsx
@@ -1,41 +1,99 @@
 import React, { useState } from 'react';
+import '../styles/ProfilePage.css';
+import Modal from 'react-modal';
 
 interface ProfileFormProps {
-  onSubmit: (name: string, email: string) => void;
   name: string;
   email: string;
 }
 
-export const ProfileForm: React.FC<ProfileFormProps> = ({ onSubmit, name, email }) => {
-  const [updatedName, setUpdatedName] = useState(name);
-  const [updatedEmail, setUpdatedEmail] = useState(email);
+const ProfileForm: React.FC<ProfileFormProps> = ({ name, email }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [newName, setNewName] = useState(name);
+  const [newEmail, setNewEmail] = useState(email);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSubmit(updatedName, updatedEmail);
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewEmail(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // fazer a chamada à API para atualizar os dados do usuário
+    // updateUserData(newName, newEmail);
+    // history.push('/profile');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Nome</label>
-        <input
-          type="text"
-          id="name"
-          value={updatedName}
-          onChange={(e) => setUpdatedName(e.target.value)}
-        />
+    <div className="profile-container">
+      <div className="profile-section">
+        <h2 className="profile-title">Meu perfil</h2>
+        <div className="profile-data">
+          <p>
+            <span className="profile-label">Nome:</span>{' '}
+            <span className="profile-value">{name}</span>
+          </p>
+          <p>
+            <span className="profile-label">Email:</span>{' '}
+            <span className="profile-value">{email}</span>
+          </p>
+        </div>
+        <div className="profile-buttons">
+          <button className="profile-button" onClick={openModal}>
+            Alterar Dados
+          </button>
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          id="email"
-          value={updatedEmail}
-          onChange={(e) => setUpdatedEmail(e.target.value)}
-        />
-      </div>
-      {/* <button type="submit">Alterar Dados</button> */}
-    </form>
-      );
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Alterar Dados Modal"
+        className="custom-modal"
+        overlayClassName="custom-OverlayClass"
+      >
+        <div className="modal-content">  
+        <h2>Alterar Dados</h2>
+          <form>
+            <label>
+              Novo Nome:
+              <input
+                type="text"
+                value={newName}
+                onChange={handleNameChange}
+              />
+            </label>
+            <label>
+              Novo Email:
+              <input
+                type="email"
+                value={newEmail}
+                onChange={handleEmailChange}
+              />
+            </label>
+            <div className="modal-buttons">
+              <button className="profile-button" onClick={handleSubmit}>
+                Salvar
+              </button>
+              <button className="profile-button" onClick={closeModal}>
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </div>
+  );
 };
+
+export default ProfileForm;
