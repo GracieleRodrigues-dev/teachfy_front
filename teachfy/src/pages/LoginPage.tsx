4d3042b33db2885f850b989194ee95b76fe89a2a
Angelina <machadoangelina321@gmail.com>
2023-06-10 19:44:05 -0300

    add os componentes da tela de login, e deletado arquivos de teste

--- teachfy/src/pages/LoginPage.tsx
+++ teachfy/src/pages/LoginPage.tsx
@@ -1,21 +1,11 @@
-import { LoginForm } from "../components/LoginForm";
-import { Link } from "react-router-dom";
+import Login from '../components/LoginForm';
 
-export function LoginPage() {
+const LoginPage = () => {
   return (
-    <div className="login-page">
-      <div className="login-container">
-        <img
-          src={require("../assets/login-image.jpg")}
-          alt="Login"
-          className="login-image"
-        />
-        <LoginForm />
-        <div className="login-links">
-          <Link to="/signup">Cadastrar-se</Link>
-          <Link to="/forgot-password">Esqueceu a senha?</Link>
-        </div>
-      </div>
+    <div>
+      <Login />
     </div>
   );
-}
+};
+
+export default LoginPage;
