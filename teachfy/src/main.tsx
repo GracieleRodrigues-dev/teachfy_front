951e2c55700f596b78337ae6a123380a237f4ed4
Angelina <machadoangelina321@gmail.com>
2023-06-26 20:56:04 -0300

    bootstrap

--- teachfy/src/main.tsx
+++ teachfy/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'; // Importe o arquivo CSS do Bootstrap
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
