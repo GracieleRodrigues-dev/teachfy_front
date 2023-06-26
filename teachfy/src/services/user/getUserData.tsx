c29fa06d7589fc1d66faa2f3ae60bf007f134026
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 13:48:09 -0300

    correção api get user

--- teachfy/src/services/user/getUserData.tsx
+++ teachfy/src/services/user/getUserData.tsx
@@ -3,7 +3,11 @@ import api from "../../plugins/axios";
 
 export const fetchUserData = async (userId: number) => {
   try {
-    const response = await api.get(`/users/${userId}`);
+    const response = (await api.get(`/users/${userId}`, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+     }));
     //const response = await axios.get(`http://localhost:8000/api/users/${userId}`);
     const { name, email } = response.data.data;
     return { name, email };
