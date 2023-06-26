3d3fea6335f63e56be0c2e68906becd881465662
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 13:27:18 -0300

    correção base url

--- teachfy/src/plugins/axios.js
+++ teachfy/src/plugins/axios.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
-
-
-
 const api = axios.create({
-
-baseURL: import.meta.env.VITE_API_BASE_URL
-
-
-
+    // baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'http://localhost:8090/',
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
 });
 
 export default api;
