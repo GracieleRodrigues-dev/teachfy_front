e61abfda353d78a0d5460c7c683c007982c71d9e
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 23:31:56 -0300

    meus decks

--- teachfy/src/components/MyDecks/MyDecksForm.tsx
+++ teachfy/src/components/MyDecks/MyDecksForm.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getMyDecks } from '../../services/decks/getMyDecks'; // Importe a função fetchUserDecks da sua API
+import '../../styles/myDecks.css';
+import { BiBookReader, BiEdit } from 'react-icons/bi';
 
 const MyDecksForm: React.FC = () => {
   const [decks, setDecks] = useState([]);
@@ -30,21 +32,39 @@ const MyDecksForm: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className='myDecks-container'>
       <h2>Meus Decks</h2>
+      <div className="row">
       {decks.map((deck: any) => (
-        <div key={deck.id}>
-          <h3>{deck.name}</h3>
-          <p>Deck público: {deck.public}</p>
-          {deck.type === 0 ? (
-            <p>Avaliativo</p>
-          ) : deck.type === 1 ? (
-            <p>Flashcard</p>
-          ) : null}
-          <button onClick={() => handleStudyClick(deck.id)}>Estudar</button>
-          <button onClick={() => handleEditClick(deck.id)}>Editar</button>
+        <div className="col-sm-2 py-3">
+          <div className="card" key={deck.id}>
+            <div className="card-body">
+              <h3 className="card-title">{deck.name}</h3>
+              <p className="card-text">
+                {deck.description}
+              </p>
+              <div className="d-flex">
+                <p className="text-muted">
+                  {deck.public === 1 ? 'Público' : "Privado"}
+                </p>
+                <p className="text-muted mx-3"> - </p>
+                <p className="text-muted">
+                  {deck.type === 1 ? 'Flashcard' : "Avaliativo"}
+                </p>
+              </div>
+            </div>
+            <div className="card-footer bg-transparent d-flex p-0">
+              <div className="div-button-card border border-white w-50">
+                <button className="button-card rounded-left" onClick={() => handleStudyClick(deck.id)}><BiBookReader/> Estudar</button>
+              </div>
+              <div className="div-button-card border border-white w-50">
+                <button className="button-card rounded-right" onClick={() => handleEditClick(deck.id)}><BiEdit/> Editar</button>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
+      </div>
     </div>
   );
 };
