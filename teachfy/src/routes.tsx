e63f9f82bc0f9bea159d53c3a4e4b702c1685e0d
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 23:55:28 -0300

    Update routes.tsx

--- teachfy/src/routes.tsx
+++ teachfy/src/routes.tsx
@@ -6,13 +6,13 @@ import { SignUpPage } from './pages/SignUpPage';
 import ProfilePage from './pages/ProfilePage';
 import LoginPage from './pages/LoginPage';
 import NewDeckPage from './pages/NewDeckPage';
-import NewDeckAvaliativoPage from './pages/NewDeckAvaliativoPage1';
+// import NewDeckAvaliativoPage from './pages/NewDeckAvaliativoPage1';
 import DeckAvaliativoPage from './pages/NewDeckAvaliativoPage';
 
 import NewDeckFlashCardPage from './pages/NewDeckFlashCardPage';
 import NewDeckFlashCardByMePage from './pages/NewDeckFlashCardByMePage';
 import NewDeckAvaliativoByMePage from './pages/NewDeckAvaliativoByMePage';
-import NewDeckAvaliativoByIAPage from './pages/NewDeckAvaliativoByIAPage ';
+// import NewDeckAvaliativoByIAPage from './pages/NewDeckAvaliativoByIAPage ';
 
 export default function AppRoutes() {
   return (
@@ -24,11 +24,11 @@ export default function AppRoutes() {
       <Route path="/login" element={<LoginPage/>}/>
       <Route path="/decks/novo-deck" element={<NewDeckPage/>}/>
       <Route path="/decks/deck/avaliativo" element={<DeckAvaliativoPage/>}/>
-      <Route path="/decks/novo-deck/avaliativo" element={<NewDeckAvaliativoPage/>}/>
+      {/* <Route path="/decks/novo-deck/avaliativo" element={<NewDeckAvaliativoPage/>}/> */}
       <Route path="/decks/novo-deck/flashcard" element={<NewDeckFlashCardPage/>}/>
       <Route path="/decks/novo-deck/flashcard/byMe" element={<NewDeckFlashCardByMePage/>}/>
       <Route path="/decks/novo-deck/avaliativo/byMe" element={<NewDeckAvaliativoByMePage/>}/>
-      <Route path="/decks/novo-deck/avaliativo/byIA" element={<NewDeckAvaliativoByIAPage/>}/>
+      {/* <Route path="/decks/novo-deck/avaliativo/byIA" element={<NewDeckAvaliativoByIAPage/>}/> */}
       {/* <Route path="/decks/novo-deck/flashcard/byAI" element={<NewDeckFlashCardPage/>}/> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
