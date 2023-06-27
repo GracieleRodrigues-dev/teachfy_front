604514d6d75b90ca12ed4d60a1be4fec7d636cd2
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 21:06:52 -0300

    bootstrap

--- teachfy/src/main.tsx
+++ teachfy/src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'; // Importe o arquivo CSS do Bootstrap
-
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
