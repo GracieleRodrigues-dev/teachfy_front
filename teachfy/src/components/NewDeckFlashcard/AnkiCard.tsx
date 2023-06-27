ef770b26a7e415c4a3a03b5b98ca14e684dbdbc2
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 22:32:53 -0300

    novo deck

--- teachfy/src/components/NewDeckFlashcard/AnkiCard.tsx
+++ teachfy/src/components/NewDeckFlashcard/AnkiCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsTrash } from 'react-icons/bs';
 
 interface AnkiCardProps {
   card: {
@@ -7,11 +8,10 @@ interface AnkiCardProps {
     backText: string;
   };
   onDelete: (cardNumber: number) => void;
-  onAdd: () => void;
   onUpdate: (cardNumber: number, position: string, value: string) => void;
 }
 
-const AnkiCard: React.FC<AnkiCardProps> = ({ card, onDelete, onAdd, onUpdate }) => {
+const AnkiCard: React.FC<AnkiCardProps> = ({ card, onDelete, onUpdate }) => {
   const handleFrontTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onUpdate(card.cardNumber, 'frontText', e.target.value);
   };
@@ -23,32 +23,38 @@ const AnkiCard: React.FC<AnkiCardProps> = ({ card, onDelete, onAdd, onUpdate }) 
   const handleDeleteClick = () => {
     onDelete(card.cardNumber);
   };
-  const handleAddClick = () => {
-    onAdd();
-  };
 
   return (
-    <div className="anki-card">
-      <div className="anki-card-header">
-        <span>{card.cardNumber}</span>
-        <button type="button" onClick={handleDeleteClick}>-</button>
-        <button type="button" onClick={handleAddClick}>+</button>
-      </div>
-      <div className="anki-card-body">
-        <label htmlFor={`frontText-${card.cardNumber}`}>Frente:</label>
-        <input
-          type="text"
-          id={`frontText-${card.cardNumber}`}
-          value={card.frontText}
-          onChange={handleFrontTextChange}
-        />
-        <label htmlFor={`backText-${card.cardNumber}`}>Verso:</label>
-        <input
-          type="text"
-          id={`backText-${card.cardNumber}`}
-          value={card.backText}
-          onChange={handleBackTextChange}
-        />
+    <div className="anki-card card mt-3 mx-0">
+      <div className="anki-card-body card-body">
+        <div className="d-flex justify-content-between">
+          <h5 className="card-title">Card {card.cardNumber}</h5>
+          <button type="button" className="btn btn-dark" onClick={handleDeleteClick}>
+            <BsTrash/>
+          </button>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <label className='form-label' htmlFor={`frontText-${card.cardNumber}`}>Frente:</label>
+            <input
+              type="text"
+              className='form-control'
+              id={`frontText-${card.cardNumber}`}
+              value={card.frontText}
+              onChange={handleFrontTextChange}
+            />
+          </div>
+          <div className="col-6">
+            <label className='form-label' htmlFor={`backText-${card.cardNumber}`}>Verso:</label>
+            <input
+              type="text"
+              className='form-control'
+              id={`backText-${card.cardNumber}`}
+              value={card.backText}
+              onChange={handleBackTextChange}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- teachfy/src/components/NewDeckFlashcard/NewDeckFlashCardByMeForm.tsx
+++ teachfy/src/components/NewDeckFlashcard/NewDeckFlashCardByMeForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AnkiCard from './AnkiCard';
 import { newDeckAnki } from '../../services/decks/newDeckAnki';
+import '../../styles/newDeck.css';
 
 interface NewDeckFlashCardByMeProps {
   title: string;
@@ -25,6 +26,8 @@ interface CardBD {
 export const NewDeckFlashCardByMeForm: React.FC<NewDeckFlashCardByMeProps> = ({ title, description,directory,isPublic, isCloneable})=> {
   const [newTitle,setTitle] = useState(title);
   const [newDescription,setDescription] = useState(description);
+  const [newPublic, setPublic] = useState(isPublic);
+  const [newCloneable, setDuplicable] = useState(isCloneable);
   const [cards, setCards] = useState<Card[]>([{ cardNumber: 1, frontText: '', backText: '' }]);
 
   const convertCardFormat = (card: Card): CardBD => {
@@ -93,42 +96,79 @@ export const NewDeckFlashCardByMeForm: React.FC<NewDeckFlashCardByMeProps> = ({ 
     setCards(updatedCards);
   };
 
+  const handleAddClick = () => {
+    handleAddCard();
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="newDeck-container">
-        <div className="newDeck-section">
-          <h2 className="newDeck-title">Novo deck de flashcard</h2>        
-          <div>
-            <label htmlFor="title">Título</label>
-            <input
-              type="text"
-              id="title"
-              value={newTitle}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="description">Descrição</label>
-            <input
-              type="text"
-              id="description"
-              value={newDescription}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          {cards.map((card) => (
-              <AnkiCard
-                key={card.cardNumber}
-                card={card}
-                onDelete={handleDeleteCard}
-                onAdd={handleAddCard}
-                onUpdate={handleUpdateCard}
-              />
-            ))}
-          </div> 
-        </div>
+    <div className="newDeck-container">
+      <div className="newDeck-section">
+        <h2 className="newDeck-title">Novo deck de flashcard</h2>   
+        <form onSubmit={handleSubmit}>    
+              <div className="row">
+              <div className="col-4">
+                <label className="form-label" htmlFor="title">Título</label>
+                <input
+                  className="form-control" 
+                  type="text"
+                  id="title"
+                  value={newTitle}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="col-8">
+                <label className="form-label" htmlFor="description">Descrição</label>
+                <input
+                  className="form-control" 
+                  type="text"
+                  id="description"
+                  value={newDescription}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div> 
+              <div className="col-10 d-flex">
+                <div className='mt-3 form-check'>
+                  <input
+                    type="checkbox"
+                    className="form-check-input" 
+                    id="newPublic"
+                    checked={newPublic}
+                    onChange={(e) => setPublic(e.target.checked)}
+                  />
+                  <label className="form-check-label" htmlFor="newPublic">Deck público</label>
+                </div>
+                <div className='mt-3 ms-5 form-check'>
+                  <input
+                    type="checkbox"
+                    className="form-check-input" 
+                    id="newCloneable"
+                    checked={newCloneable}
+                    onChange={(e) => setDuplicable(e.target.checked)}
+                  />
+                  <label className="form-label" htmlFor="newCloneable">Permite duplicação</label>
+                </div>
+              </div>
+              <div className="col-2 justify-content-end d-flex align-items-end">
+                <div>
+                  <button type="button" className="btn btn-dark btn-sm" onClick={handleAddClick}>Adicionar Card</button>
+                </div>
+              </div>
+              <div className="col-12">
+                {cards.map((card) => (
+                    <AnkiCard
+                      key={card.cardNumber}
+                      card={card}
+                      onDelete={handleDeleteCard}
+                      onUpdate={handleUpdateCard}
+                    />
+                  ))}
+              </div>
+              <div className="col-12 d-flex justify-content-end mt-3">
+                <button className='btn btn-dark'>Salvar</button>
+              </div>
+            </div> 
+        </form>
       </div>
-      <button>Salvar</button>
-    </form>
+    </div>
   );
 };
